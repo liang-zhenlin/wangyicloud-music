b5592a332b2ec89578b8f2bd2e9798eafe317e84
liang <liangzhenlin0409@163.com>
2025-02-25 20:13:30 +0800

    添加 eslint prettier craco 等配置

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 
 const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
-    webpack: {
-        alias: {
-            '@': resolve('src')
-        }
+  plugins: [{ plugin: CracoLessPlugin }],
+  webpack: {
+    alias: {
+      '@': resolve('src')
     }
+  }
 }
